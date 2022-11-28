daa7de81cf95c64422cfbf4cd3cd3858faf51462
Annie S <annie.m.stanley@gmail.com>
2022-11-27 22:14:01 -0500

    Updated api helper functions to include google places

--- api_helper_functions.py
+++ api_helper_functions.py
@@ -18,12 +18,6 @@ def fsq_instructions():
     print('Function assumes location dataframe is named \'nyc_geo_df\' & that latitudes are in [2] and longitudes are in [3]')
 
 
-# In[12]:
-
-
-fsq_instructions()
-
-
 # In[ ]:
 
 
@@ -154,4 +148,122 @@ def yelp_locations (lat, lon, cat):
     yelp_df = df
     #print(yelp_response)
     return
+
+
+# In[ ]:
+
+
+def yelp_api_loop(cat):
+    # Loop for iterating fsq_location function throughout geographic dataframe
+    """
+    Iterates through the nyc_geo_df, and the yelp_locations function.
+    
+    Parameters:
+        cat (str): desired POI category
+    Returns:
+        yelp_df: a dataframe containing results from the api request
+
+    """
+    num_rows = nyc_geo_df.shape[0]
+    row_count = 0
+
+
+    while row_count < num_rows:
+        yelp_locations((nyc_geo_df.iloc[row_count,2]),(nyc_geo_df.iloc[row_count,3]),cat)
+        row_count +=1
+
+
+# In[ ]:
+
+
+# Google Places Api
+
+
+# In[ ]:
+
+
+def gp_instructions():
+    print('Reset the json response using "gp_data_resto=[]" before running api_loop so the dataframe doesn\'t stack if you run the loop again')
+    print('-----')
+    print('Set your yelp API key as "gp_secret = os.environ[\'GP_API\']"')
+    print('-----')
+    print('Function assumes location dataframe is named \'nyc_geo_df\' & that latitude & longitude are combined together in [4]')
+
+
+# In[ ]:
+
+
+# restaurant category in google places = restaurant
+# function for accessing desired information from the Google Places API
+
+def gp_locations (lat_lon, cat):
+    
+    """
+        Returns a dataframe containing parsed json on a given location
+    
+    Parameters:
+        lat_lon (str): The latitude & longitude of the search area (pre-formatted to fit google's preferred formatting)
+        cat (str): The poi category to return information on
+    
+    Returns:
+        necessary framework to run the api_loop function
+    """
+    # construct url
+    url= 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + lat_lon + '&radius=1000&type='+ cat + '&key=' + gp_secret
+    
+    # create dictionary for headers
+    headers = {}
+    #payload = {}
+        
+    # perform get request
+    response = requests.get(url, headers=headers)
+
+    # process request into usable JSON file
+    gp_response = json.loads(response.text)
+    gp_data_resto.append(gp_response)
+    
+    # converts normalized JSON into a dataframe
+    df = pd.json_normalize(gp_data_resto,['results'])
+    df['req_cat'] = cat
+    
+    # convert dataframe to global variable that can be accessed outside of the function
+    global gp_df
+    gp_df = df
+    
+    return
+
+
+# In[ ]:
+
+
+def gp_api_loop(cat):
+    # Loop for iterating fsq_location function throughout geographic dataframe
+    """
+    Iterates through the nyc_geo_df, and the gp_locations function.
+    
+    Parameters:
+        cat (str): desired POI category
+    Returns:
+        gp_df: a dataframe containing results from the api request
+
+    """
+    num_rows = nyc_geo_df.shape[0]
+    row_count = 0
+
+
+    while row_count < num_rows:
+        gp_locations((nyc_geo_df.iloc[row_count,4]),cat)
+        row_count +=1
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
